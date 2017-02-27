a7670ecb89bc8841db4a13d4e36556d13108572a
Luke Campbell <luke.s.campbell@gmail.com>
2017-02-27 16:48:44 -0500

    Refactor dac status

--- status/tasks.py
+++ status/tasks.py
@@ -22,6 +22,7 @@ logger.setLevel(logging.DEBUG)
 STATUS_FAIL = -1
 STATUS_OK = 0
 
+
 def write_json(data):
     '''
     Returns true if data was successfully written to JSON file
@@ -38,12 +39,13 @@ def write_json(data):
     logger.info('Updated %s', json_file)
     return True
 
+
 def get_trajectory(erddap_url):
     '''
     Reads the trajectory information from ERDDAP and returns a GEOJSON like
     structure.
     '''
-    #http://data.ioos.us/gliders/erddap/tabledap/ru01-20140104T1621.json?latitude,longitude&trajectory=%22ru01-20140104T1621%22
+    # http://data.ioos.us/gliders/erddap/tabledap/ru01-20140104T1621.json?latitude,longitude&trajectory=%22ru01-20140104T1621%22
     url = erddap_url.replace('html', 'json')
     url += '?longitude,latitude'
     response = requests.get(url)
@@ -51,16 +53,17 @@ def get_trajectory(erddap_url):
         raise IOError("Failed to get trajectories")
     data = response.json()
     geo_data = {
-        'type' : 'LineString',
-        'coordinates' : data['table']['rows']
+        'type': 'LineString',
+        'coordinates': data['table']['rows']
     }
     return geo_data
+
 
 def write_trajectory(deployment, geo_data):
     '''
     Writes a geojson like python structure to the appropriate data file
     '''
-    trajectory_dir  = app.config.get('TRAJECTORY_DIR')
+    trajectory_dir = app.config.get('TRAJECTORY_DIR')
     username = deployment['username']
     name = deployment['name']
     dir_path = os.path.join(trajectory_dir, username)
@@ -73,7 +76,7 @@ def write_trajectory(deployment, geo_data):
 
 @celery.task()
 def get_trajectory_features():
-    erddap_url      = app.config.get('ERDDAP_URL')
+    erddap_url = app.config.get('ERDDAP_URL')
     deployments_url = app.config.get('DAC_API')
 
     response = requests.get(deployments_url)
@@ -87,7 +90,8 @@ def get_trajectory_features():
             geo_data = get_trajectory(d['erddap'])
             write_trajectory(d, geo_data)
         except IOError:
-            logger.exception("Failed to get trajectory for %s", d['deployment_dir'])
+            logger.exception("Failed to get trajectory for %s",
+                             d['deployment_dir'])
 
     return deployments
 
@@ -99,46 +103,50 @@ def get_dac_status():
     deployment_url_template = 'http://data.ioos.us/gliders/providers/deployment/{:s}'
     tds_url_template = 'http://data.ioos.us/gliders/thredds/dodsC/deployments/{:s}/{:s}/catalog.html?dataset=deployments/{:s}/{:s}/{:s}.nc3.nc'
     deployments = {
-         'meta' : {
-            'fetch_time' : time.strftime('%b %d, %Y %H:%M Z', time.gmtime())
+        'meta': {
+            'fetch_time': time.strftime('%b %d, %Y %H:%M Z', time.gmtime())
         },
-        'datasets' : []
+        'datasets': []
     }
 
     variables = {
-        'datasetID' : 'datasetID',
-        'institution' : 'institution',
-        'title' : 'title',
-        'minLongitude' : 'west',
-        'maxLongitude' : 'east',
-        'minLatitude' : 'south',
-        'maxLatitude' : 'north',
-        'minTime' : 'ts0',
-        'maxTime' : 'ts1',
-        'subset' : 'subset',
-        'tabledap' : 'tabledap',
-        'MakeAGraph' : 'graph',
-        'fgdc' : 'fgdc',
-        'metadata' : 'meta',
-        'rss' : 'rss',
-        'summary' : 'summary'
+        'datasetID': 'datasetID',
+        'institution': 'institution',
+        'title': 'title',
+        'minLongitude': 'west',
+        'maxLongitude': 'east',
+        'minLatitude': 'south',
+        'maxLatitude': 'north',
+        'minTime': 'ts0',
+        'maxTime': 'ts1',
+        'subset': 'subset',
+        'tabledap': 'tabledap',
+        'MakeAGraph': 'graph',
+        'fgdc': 'fgdc',
+        'metadata': 'meta',
+        'rss': 'rss',
+        'summary': 'summary'
     }
     columns = variables.keys()
     # Time coverage regexs
-    t0_re = re.compile('time_coverage_start\s"(\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z)"')
-    t1_re = re.compile('time_coverage_end\s"(\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z)"')
+    t0_re = re.compile(
+        'time_coverage_start\s"(\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z)"')
+    t1_re = re.compile(
+        'time_coverage_end\s"(\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z)"')
     # Request the dac deployments metadata
     logger.info('Fetching DAC deployments: %s', dac_api_url)
     dac_request = requests.get(dac_api_url)
     if dac_request.status_code != 200:
-        logger.error('ERDDAP request failed: %s (%s)', dac_api_url, dac_request.reason)
+        logger.error('ERDDAP request failed: %s (%s)',
+                     dac_api_url, dac_request.reason)
         return False
 
     # Request the ERDDAP dataset metadata
     logger.info('Fetching ERDDAP datasets: %s', erddap_url)
     erddap_request = requests.get(erddap_url)
     if erddap_request.status_code != 200:
-        logger.error('DAC request failed: %s (%s)', erddap_url, erddap_request.reason)
+        logger.error('DAC request failed: %s (%s)',
+                     erddap_url, erddap_request.reason)
         return False
 
     # Fetch the results from both requests
@@ -168,7 +176,7 @@ def get_dac_status():
     for dac_record in dac_data:
 
         # Initialize the metadata record
-        meta = {variables[column] : None for column in columns}
+        meta = {variables[column]: None for column in columns}
 
         # Initialize a few other keys
         meta['status'] = None
@@ -185,7 +193,7 @@ def get_dac_status():
         # If the dac deployment name is in the ERDDAP dataset_ids, make an ERDDAP
         # request and fill in the missing metadata
         if dac_record['name'] in dataset_ids:
-            i = dataset_ids.index(dac_record['name']) 
+            i = dataset_ids.index(dac_record['name'])
             for column in columns:
                 col_id = erddap_data['columnNames'].index(column)
                 meta[variables[column]] = erddap_data['rows'][i][col_id]
@@ -197,22 +205,25 @@ def get_dac_status():
             logger.info('Fetching das: %s', das_url)
             das_request = requests.get(das_url)
             if das_request.status_code != 200:
-                logger.error('das request failed: %s (%s)', das_url, das_request.reason)
+                logger.error('das request failed: %s (%s)',
+                             das_url, das_request.reason)
                 continue
 
             # Parse the global:time_coverage_start attribute
             t0_match = t0_re.search(das_request.text)
             t0_epoch = None
             if not t0_match:
-                logger.error('%s: No time_coverage_start regex match', dataset_id)
+                logger.error(
+                    '%s: No time_coverage_start regex match', dataset_id)
             else:
                 t0_epoch = clock.erddap_ts2epoch(t0_match.groups()[0])
 
-            # Parse the global:time_coverage_end attribute    
+            # Parse the global:time_coverage_end attribute
             t1_match = t1_re.search(das_request.text)
             t1_epoch = None
             if not t1_match:
-                logger.error('%s: No time_coverage_end regex match', dataset_id)
+                logger.error(
+                    '%s: No time_coverage_end regex match', dataset_id)
             else:
                 # Convert the time_coverage_end to epoch seconds and set the bootstrap
                 # contextual class
@@ -249,10 +260,10 @@ def get_dac_status():
 
         # Try to fetch the THREDDS .das to see if the dataset exists
         tds_das_url = tds_url_template.format(meta['username'],
-            meta['name'],
-            meta['username'],
-            meta['name'],
-            meta['name'])
+                                              meta['name'],
+                                              meta['username'],
+                                              meta['name'],
+                                              meta['name'])
         logger.info('Fetching THREDDS catalog: %s', tds_das_url)
         tds_request = requests.get(tds_das_url)
         meta['tds'] = None
@@ -260,7 +271,8 @@ def get_dac_status():
             meta['tds'] = tds_das_url.replace('.das', '.html')
 
         # Add the deployment metadata to the return object
-        deployments['datasets'].append(collections.OrderedDict(sorted(meta.items(), key=lambda t: t[0])))
+        deployments['datasets'].append(collections.OrderedDict(
+            sorted(meta.items(), key=lambda t: t[0])))
 
     status = write_json(deployments)
     return status
